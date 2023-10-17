c1cce535ebebdf82b6d12124a482ce7380b589fb
unknown <nomgnenoel@gmail.com>
2023-10-17 14:26:13 +0100

    create Book class

--- classes/book.rb
+++ classes/book.rb
@@ -1,1 +1,15 @@
-puts 'class'
+require_relative 'item'
+
+class Book < Item
+  attr_accessor :publisher :cover_state
+
+  def initialize(publish_date, publisher, cover_state, archived: false)
+    super(publish_date, archived: archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || bad_cover_state
+  end
+end
