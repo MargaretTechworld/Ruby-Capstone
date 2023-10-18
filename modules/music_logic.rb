33f24d674b47622768c151d0c6d2d3038b1de404
Toyyib <hamzatbolaji648@yahoo.com>
2023-10-18 02:44:22 +0100

    genre_music_classes_and_methods

--- modules/music_logic.rb
+++ modules/music_logic.rb
@@ -15,29 +15,29 @@ module MusicLibrary
         puts 'Creating a music album... Add details below.'
         print 'Album Name: '
         name = gets.chomp
-    
+
         print 'Publish Date: '
         publish_date = gets.chomp
-    
+
         print 'Cover State (good, bad, etc.): '
         cover_state = gets.chomp
-    
+
         print 'On Spotify (true/false): '
         on_spotify = gets.chomp.downcase == 'true'
-    
+
         print 'Archived (true/false): '
         archived = gets.chomp.downcase == 'true'
-    
+
         album = MusicAlbum.new(name, publish_date, cover_state, on_spotify, archived: archived)
         @albums << album
-    
+
         puts 'Music album created successfully'
-    
+
         puts 'What genre is the above music album?'
         genre_name = gets.chomp
-    
+
         existing_genre = @genres.find { |g| g.name == genre_name }
-    
+
         if existing_genre
           album.genre = existing_genre
         else
@@ -47,4 +47,3 @@ module MusicLibrary
         end
     end
 end
-    
