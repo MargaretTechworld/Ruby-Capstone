06a0db1fc2f4374ae9315ba8ff17c523dcc4653e
Toyyib <hamzatbolaji648@yahoo.com>
2023-10-18 13:55:19 +0100

    Test

--- classes/music.rb
+++ classes/music.rb
@@ -7,6 +7,7 @@ class MusicAlbum < Item
     super(publish_date, archived: archived)
     @id = Random.rand(1..1000)
     @name = name
+    @publish_date = format_publish_date(publish_date)
     @cover_state = cover_state
     @on_spotify = on_spotify
   end
@@ -24,5 +25,18 @@ class MusicAlbum < Item
 
   def can_be_archived?
     supper && @on_spotify
+  end
+
+  private
+
+  def format_publish_date(date)
+    # Check if the date is in the "YYYY-MM-DD" format; if not, format it
+    if date.match?(/\A\d{4}-\d{2}-\d{2}\z/)
+      date
+    else
+      # Format the date as "YYYY-MM-DD" or handle any other formatting logic as needed
+      # For example, if the date is "03/01/2022," you can format it as follows:
+      Date.parse(date).strftime('%Y-%m-%d')
+    end
   end
 end
